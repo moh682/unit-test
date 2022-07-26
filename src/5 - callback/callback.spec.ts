586586c6e0a6a78688aa1b387c73e460d7301510
Mohammad Hariri <moh.050196@gmail.com>
2022-07-27 01:05:10 +0200

    finish callback tests

--- src/5 - callback/callback.spec.ts
+++ src/5 - callback/callback.spec.ts
@@ -1,3 +1,12 @@
-import { describe, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
+import { delayyer } from './callback';
 
-describe('callback', () => {});
+const myCallback = vi.fn();
+
+describe('callback', () => {
+  it('should execute callback function', async () => {
+    await delayyer(myCallback);
+
+    expect(myCallback).toHaveBeenCalledOnce;
+  });
+});
